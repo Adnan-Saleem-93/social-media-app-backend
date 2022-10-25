71261cd8a024d6af86da68035628444b40f448f6
Adnan Saleem <adnan13893@gmail.com>
2022-10-25 22:38:34 +0500

    connected to mongodb server

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import {CONNECTION_URL, PORT} from './utils/constants.js'
 
 const app = express()
 
 app.use(bodyParser.json({limit: '30mb'}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`DB Connected. Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message))
---
+++ utils/constants.js
@@ -0,0 +1,3 @@
+export const CONNECTION_URL =
+  'mongodb+srv://Chamber1881:8x8pN64PZ9Adr8iyRqgf@cluster0.so5iutd.mongodb.net/?retryWrites=true&w=majority'
+export const PORT = process.env.PORT || 5000
