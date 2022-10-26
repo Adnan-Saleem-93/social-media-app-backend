a45427b638edd4f47fb80d7c9d74c80b3061fb49
Adnan Saleem <adnan13893@gmail.com>
2022-10-26 22:47:34 +0500

    created routes folder and added posts router

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import {CONNECTION_URL, PORT} from './utils/constants.js'
+import postRoutes from './routes/posts.js'
 
 const app = express()
 
+app.use('/posts', postRoutes)
 app.use(bodyParser.json({limit: '30mb'}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
