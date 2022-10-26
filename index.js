8d6056b677ece785cf04d5cf9aadc3c506b6014c
Adnan Saleem <adnan13893@gmail.com>
2022-10-26 22:58:49 +0500

    created and used environment variables

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import {CONNECTION_URL, PORT} from './utils/constants.js'
+
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv'
 
 const app = express()
 
@@ -12,9 +13,13 @@ app.use(bodyParser.json({limit: '30mb'}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
 
+dotenv.config({path: './.env'})
+
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
-    app.listen(PORT, () => console.log(`DB Connected. Server is running on port: ${PORT}`))
+    app.listen(process.env.PORT, () =>
+      console.log(`DB Connected. Server is running on port: ${process.env.PORT}`)
+    )
   )
   .catch((error) => console.log(error.message))
