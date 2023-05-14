57bee88fccc818e0d7aefebebc73547cecf9a277
Adnan Saleem <adnan13893@gmail.com>
2023-05-14 22:35:30 +0500

    fixed authentication issue

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,13 +1,14 @@
 import {verify} from 'jsonwebtoken'
-import PostMessage from '../models/posts.js'
+import User from '../models/auth.js'
 
 export const authorizeRequest = async (req, res, next) => {
   try {
     const token = req.headers?.authorization?.split(' ')[1]
     if (token) {
-      const decodedToken = verify(token, 'RANDOM_TOKEN_SECRET')
-      const userId = decodedToken?._id
-      const result = await PostMessage.findById(userId)
+      const decodedToken = verify(token, 'new_user_private_key')
+
+      const userId = decodedToken?.id
+      const result = await User.findById(userId)
 
       if (!result) {
         res.status(401).json({
